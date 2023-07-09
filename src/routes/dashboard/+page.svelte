8f604f5931599c63def17b1f8bf1b51291076a1d
idkfelix <felix.coulthard@gmail.com>
2023-07-09 22:01:27 +1000

    init

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+  import { user } from "$lib/firebase";
+</script>
+
+{#if !$user}
+  {goto('/login')}
+{/if}
+
 <h1>Dashboard</h1>
+<p>{$user?.uid}</p>
